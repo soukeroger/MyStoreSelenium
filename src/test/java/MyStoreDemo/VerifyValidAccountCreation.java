fd3153eeb1183057bf062f283cf44037ceea811b
ROGER Souke <roger.souke-cic-uk@ibm.com>
2018-09-02 17:04:17 +0100

    Addedd

--- src/test/java/MyStoreDemo/VerifyValidAccountCreation.java
+++ src/test/java/MyStoreDemo/VerifyValidAccountCreation.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.MyStoreLandingPageNew;
 
+import java.util.concurrent.TimeUnit;
+
 public class VerifyValidAccountCreation {
 
 
@@ -16,6 +18,7 @@ public class VerifyValidAccountCreation {
             System.setProperty("webdriver.gecko.driver", "C:\\workspace\\geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
             driver.navigate().to("http://automationpractice.com/index.php");
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 //            WebDriverWait wait = new WebDriverWait(driver, 100);
 
