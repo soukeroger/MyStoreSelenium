5324e236975a387a73e294ca8cf46312af83c05a
ROGER Souke <roger.souke-cic-uk@ibm.com>
2018-09-04 20:09:38 +0100

    POM extension

--- src/test/java/MyStoreDemo/VerifyValidAccountCreation.java
+++ src/test/java/MyStoreDemo/VerifyValidAccountCreation.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import pages.DropDownPage;
 import pages.MyStoreLandingPageNew;
 
 import java.util.concurrent.TimeUnit;
@@ -13,7 +14,7 @@ public class VerifyValidAccountCreation {
 
 
         @Test
-        public void createAccount() {
+        public void createAccount() throws InterruptedException {
             System.setProperty("webdriver.gecko.driver", "C:\\workspace\\geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
             driver.navigate().to("http://automationpractice.com/index.php");
@@ -21,6 +22,13 @@ public class VerifyValidAccountCreation {
 
             MyStoreLandingPageNew sign_in_page = PageFactory.initElements(driver,MyStoreLandingPageNew.class);
             sign_in_page.createAccount("signIn@demo.com", "rabit", "Whisl", "dfdsfs", "23");
+
+            DropDownPage chooseDays = PageFactory.initElements(driver, DropDownPage.class);
+            chooseDays.setDays();
+            DropDownPage chooseMnths = PageFactory.initElements(driver, DropDownPage.class);
+            chooseMnths.setMonths();
+
+
 
         }
     }
